f1ed519d9d12f09f775653e91ebecfdbb9777f20
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2025-03-04 19:19:22 +0100

    refactor NewsletterSection component with updated styling and content for better user experience

--- src/components/NewsletterSection/NewsletterSection.jsx
+++ src/components/NewsletterSection/NewsletterSection.jsx
@@ -12,11 +12,14 @@ const NewsletterSection = () => {
   return (
     <section className={styles.newsletterSection}>
       <div className={styles.newsletterContent}>
-        <h2>Nyhedesbrev</h2>
-        <p>
+        <h2>
+          Nyhedesbrev
+          <p>Få nyhederne fra gården på din mail.</p>
+        </h2>
+        <small>
           Tilmeld dig vores nyhedesbrev - så kan du altid følgle med i, hvad der
           sker på farmen.
-        </p>
+        </small>
       </div>
 
       <form onSubmit={handleSubmit}>
@@ -24,13 +27,13 @@ const NewsletterSection = () => {
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email"
+          placeholder="Din email"
           required
           className={styles.input}
         />
-        
-        <button type="submit" className={styles.button}>
-          Subscribe
+
+        <button type="submit" className={styles.subscriptionButton}>
+          Tilmeld
         </button>
       </form>
     </section>
