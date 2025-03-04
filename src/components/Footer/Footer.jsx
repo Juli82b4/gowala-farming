86cf21f97065456b8172287fcedad1efb67f2e79
Juliya Christendian Sriraj <157354868+Juli82b4@users.noreply.github.com>
2025-03-04 22:56:42 +0100

    add Footer component with styling and contact information (#18)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import Logo from "../Logo/Logo";
+import styles from "./Footer.module.css";
+import { FaPhone, FaClock, FaMapMarkerAlt } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={styles.footer}>
+      <Logo className={styles.footerLogo} />
+      <div className={styles.footerInfo}>
+        <p>
+          Gowala Farms er en dedikeret gård, der producerer friske
+          mejeriprodukter og kvalitetskød med fokus på dyrevelfærd,
+          bæredygtighed og autentisk smag.
+        </p>
+        <div className={styles.footerIcons}>
+          <div>
+            <FaPhone />
+            <small>
+              +88130-589-745-6987
+              <br />+ 1655-456-532
+            </small>
+            <br />
+            <br />
+          </div>
+          <div>
+            <FaClock />
+            <small>
+              Man-Fre 09:00 - 18:00 <br />
+              (undtagen helligdage)
+            </small>
+            <br />
+            <br />
+          </div>
+          <div>
+            <FaMapMarkerAlt />
+            <small>
+              Mejerigade 14 <br />
+              Mejeby
+            </small>
+            <br />
+            <br />
+          </div>
+        </div>
+      </div>
 
-export default Footer
+      <div className={styles.footerCopyright}>
+        © 2024 <span>Gowala</span>. All rights Reserved By <br/>
+        <span>LabArtisian</span> & <span>Viborg Media College</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
