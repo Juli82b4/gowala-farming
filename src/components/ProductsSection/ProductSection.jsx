4e65b6e56c8b4acb5e997922f70a9a25d82ef816
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2025-03-04 15:48:27 +0100

    dv

--- src/components/ProductsSection/ProductSection.jsx
+++ src/components/ProductsSection/ProductSection.jsx
@@ -16,13 +16,26 @@ export const ProductSection = () => {
       </div>
 
       <div className={styles.products}>
-        {products.map((product) => (
-          <div key={product._id} className={styles.product}>
-            <img src={product.image} alt={product.title} />
-            <h2>{product.title}</h2>
-            <h3>${product.price}</h3>
-          </div>
-        ))}
+        {products.length > 0 ? (
+          products.map((product) => (
+            <div key={product._id} className={styles.product}>
+              <img
+                src={product.image}
+                alt={product.title}
+                className={styles.productImage}
+              />
+              <div className={styles.productDetails}>
+                <h2>{product.title}</h2>
+                <p className={styles.productDescription}>
+                  {product.description}
+                </p>
+                <h3>${product.price.toFixed(2)}</h3>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className={styles.noProducts}>No products available</div>
+        )}
       </div>
     </div>
   );
