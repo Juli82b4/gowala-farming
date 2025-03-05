839a2322f51b0c57bd26a6d4662ae0ca12129f0c
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2025-03-05 13:07:06 +0100

    refactor ProductsPage and ProductSection components; update routing to /shop and enhance product display with new header and styling

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-
 import Navigation from "../components/Navigation/Navigation";
 import ProductSection from "../components/ProductsSection/ProductSection";
 import Footer from "../components/Footer/Footer";
+import HeaderHeading from "../components/HeaderHeading/HeaderHeading";
 
 const ProductsPage = () => {
   return (
     <>
       <Navigation />
-      <ProductSection />
+      <HeaderHeading
+        headingtext="Gowala Shopping"
+        subtext="Vi er taknemmelige for dit bidrag"
+      />
+      <ProductSection
+        productHeading="Alle vores produkter"
+        subheading="Alt på ét sted"
+        productsubtext="Her på siden finder du alle vores friske mejeriprodukter og kvalitetskød fra Gowala Farms – direkte fra gården til dit bord."
+        displayAllProducts={true} // Pass the prop to display all products
+      />
       <Footer />
     </>
   );
