4ee219fac94f893753aff9d71f0048a1f3c55554
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2025-03-05 11:54:15 +0100

    add ProductsPage component and routing; integrate navigation and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import MainPage from "./Pages/MainPage";
 import { useRoutes } from "react-router-dom";
+import ProductsPage from "./Pages/ProductsPage";
 
 function App() {
   const routes = useRoutes([
     { path: "/", element: <MainPage /> },
-    { path: "/", element: <MainPage /> },
+    { path: "/products", element: <ProductsPage /> },
   ]);
   return (
     <div className="app">
---
+++ src/Pages/ProductsPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Navigation from "../components/Navigation/Navigation";
+import ProductSection from "../components/ProductsSection/ProductSection";
+import Footer from "../components/Footer/Footer";
+
+const ProductsPage = () => {
+  return (
+    <>
+      <Navigation />
+      <ProductSection />
+      <Footer />
+    </>
+  );
+};
+
+export default ProductsPage;
