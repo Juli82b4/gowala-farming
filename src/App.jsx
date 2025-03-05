f810282920db2a653c57c2a282dc3215efc5db0f
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2025-03-05 13:15:27 +0100

    add ServiceArticlePage component and routing; include navigation, header, newsletter, and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import MainPage from "./Pages/MainPage";
 import { useRoutes } from "react-router-dom";
 import ProductsPage from "./Pages/ProductsPage";
+import ServiceArticlePage from "./Pages/ServiceArticlePage";
 
 function App() {
   const routes = useRoutes([
     { path: "/", element: <MainPage /> },
     { path: "/shop", element: <ProductsPage /> },
+    { path: "/services", element: <ServiceArticlePage /> },
   ]);
   return (
     <div className="app">
---
+++ src/Pages/ServiceArticlePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navigation from "../components/Navigation/Navigation";
+import HeaderHeading from "../components/HeaderHeading/HeaderHeading";
+import NewsletterSection from "../components/NewsletterSection/NewsletterSection";
+import Footer from "../components/Footer/Footer";
+
+const ServiceArticlePage = () => {
+  return (
+    <>
+      <Navigation />
+      <HeaderHeading />
+      <NewsletterSection />
+      <Footer />
+    </>
+  );
+};
+
+export default ServiceArticlePage;
