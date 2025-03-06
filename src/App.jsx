2d8da4e88ff3466268c21f7a9b01a732cfff330a
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2025-03-06 15:40:46 +0100

    add authentication context and login component; implement user sign-in functionality and token management

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Login from "./components/login/Login";
 import MainPage from "./Pages/MainPage";
 import { useRoutes } from "react-router-dom";
 import ProductsPage from "./Pages/ProductsPage";
@@ -16,8 +17,9 @@ function App() {
     { path: "/services", element: <ServiceArticlePage /> },
     { path: "/about", element: <AboutPage /> },
     { path: "/contact", element: <ContactPage /> },
-    { path: "/checkout", element: <CheckoutPage/>},
-    { path: "/subscribe", element: <SubscribePage/>},
+    { path: "/checkout", element: <CheckoutPage /> },
+    { path: "/subscribe", element: <SubscribePage /> },
+    { path: "/login", element: <Login /> },
   ]);
   return (
     <div className="app">
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,89 @@
+import { useLocalStorage } from "@uidotdev/usehooks";
+import { createContext, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate, useLocation } from "react-router-dom";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [auth, saveAuth] = useLocalStorage("auth", {});
+  const [user, setUser] = useLocalStorage("user", {});
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      if (
+        location.pathname.includes("backoffice") &&
+        !location.pathname.includes("login")
+      ) {
+        if (auth.token !== undefined) {
+          let response = await fetch("http://localhost:3042/auth/token", {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${auth.token}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ token: auth.token }),
+          });
+
+          let result = await response.json();
+
+          if (result.message === "Token Expired") {
+            saveAuth({});
+            setUser({});
+
+            return navigate("/login");
+          } else {
+            setUser(result.data);
+          }
+        } else {
+          return navigate("/login");
+        }
+      }
+    };
+
+    checkUser();
+  }, [location.pathname, auth.token, navigate, saveAuth]);
+
+  const token = auth.token ? auth.token : "";
+
+  const signedIn = auth.token !== undefined;
+
+  const signIn = async (email, password) => {
+    let response = await fetch("http://localhost:3042/auth/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+
+    let result = await response.json();
+
+    const user = jwtDecode(result.data.token);
+
+    saveAuth({ token: result.data.token });
+    setUser(user);
+    navigate("/");
+
+    return user;
+  };
+
+  const getUser = () => {
+    return token !== "" ? jwtDecode(token) : {};
+  };
+
+  const signOut = () => {
+    saveAuth({});
+    setUser({});
+  };
+
+  const value = { token, user, getUser, signIn, signOut, signedIn };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
